bb49d3007df14f19a3810cc52e24231fbb19f7ad
Bugo <cheetah@bk.ru>
2024-01-05 10:46:28 +0500

    Translate .vitepress/theme/components/TextAd.vue

--- .vitepress/theme/components/TextAd.vue
+++ .vitepress/theme/components/TextAd.vue
@@ -6,13 +6,14 @@ function track() {
 
 <template>
   <div class="text-ad">
-    <span class="label">Ad</span>
+    <span class="label">Реклама</span>
     <a
       href="https://aircode.io"
       target="_blank"
       rel="noopener nofollow"
       @click="track"
-      >Try it → Build and deploy your own ChatGPT bot with JavaScript in 5 minutes</a
+      >Попробуйте → Создайте и разверните собственного бота ChatGPT с
+      помощью JavaScript за 5 минут</a
     >
   </div>
 </template>
