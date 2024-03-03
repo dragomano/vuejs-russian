e626401f33ba96cb06bf11e7f4db27682f02f0f7
Bugo <cheetah@bk.ru>
2024-03-03 08:53:48 +0500

    Translate search form UI

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -689,9 +689,50 @@ export default defineConfigWithTheme<ThemeConfig>({
     algolia: {
       indexName: 'vuejs',
       appId: 'ML0LEBN7FQ',
+      //appId: 'C0MWTQQP1U',
       apiKey: 'f49cbd92a74532cc55cfbffa5e5a7d01',
+      //apiKey: 'e675323c31c84a6558df483794ab764b',
       searchParameters: {
         facetFilters: ['version:v3']
+      },
+      placeholder: 'Поиск в документации',
+      translations: {
+        button: {
+          buttonText: 'Поиск',
+          buttonAriaLabel: 'Поиск'
+        },
+        modal: {
+          searchBox: {
+            resetButtonTitle: 'Сбросить поиск',
+            resetButtonAriaLabel: 'Сбросить поиск',
+            cancelButtonText: 'Отменить поиск',
+            cancelButtonAriaLabel: 'Отменить поиск'
+          },
+          startScreen: {
+            recentSearchesTitle: 'История поиска',
+            noRecentSearchesText: 'Нет истории поиска',
+            saveRecentSearchButtonTitle: 'Сохранить в истории поиска',
+            removeRecentSearchButtonTitle: 'Удалить из истории поиска',
+            favoriteSearchesTitle: 'Избранное',
+            removeFavoriteSearchButtonTitle: 'Удалить из избранного'
+          },
+          errorScreen: {
+            titleText: 'Невозможно получить результаты',
+            helpText: 'Вам может потребоваться проверить подключение к Интернету'
+          },
+          footer: {
+            selectText: 'выбрать',
+            navigateText: 'перейти',
+            closeText: 'закрыть',
+            searchByText: 'поставщик поиска'
+          },
+          noResultsScreen: {
+            noResultsText: 'Нет результатов для',
+            suggestedQueryText: 'Вы можете попытаться узнать',
+            reportMissingResultsText: 'Считаете, что поиск даёт ложные результаты？',
+            reportMissingResultsLinkText: 'Нажмите на кнопку «Обратная связь»'
+          }
+        }
       }
     },
 
