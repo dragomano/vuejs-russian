320477b772b40354029e90b2a1c9c7993b212b0f
Bugo <cheetah@bk.ru>
2025-03-10 19:25:39 +0500

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -646,7 +646,7 @@ export default defineConfigWithTheme<ThemeConfig>({
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "la33dit45x")`,
+      })(window, document, "clarity", "script", "la33dit45x")`,
     ],
   ],
 
